48f75239ace0bc596f092419d84277c622768d8c
Doraemonls <syyzls@gmail.com>
2017-01-07 21:30:44 +0800

    Fix icon bug.

--- web/ditu.js
+++ web/ditu.js
@@ -3,44 +3,32 @@ var mapObj = new AMap.Map('mapcnt', {
   zoom: 8
 });
 
-var icon = new AMap.Icon({
-  size: new AMap.Size(16, 16)
-});
-
 var marker1 = new AMap.Marker({
-  icon: icon, //24px*24px
   position: [116.320265,39.894062],
   map: mapObj
 });
 
 var marker2 = new AMap.Marker({
-  icon: icon, //24px*24px
   position: [116.488552,39.98057],
   map: mapObj
 });
 
 var marker3 = new AMap.Marker({
-  icon: icon, //24px*24px
   position: [116.474894,40.012378],
   map: mapObj
 });
 
 var marker4 = new AMap.Marker({
-  icon: icon, //24px*24px
   position: [116.460899,39.990007],
   map: mapObj
 });
 
 var marker5 = new AMap.Marker({
-  icon: icon, //24px*24px
   position: [116.491926,39.926322],
   map: mapObj
 });
 
 var marker6 = new AMap.Marker({
-  icon: icon, //24px*24px
   position: [116.310319,39.97937],
   map: mapObj
 });
-
-

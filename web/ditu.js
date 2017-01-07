77d8833bd2f7429fade7f3a186393a0ba9d71daf
Doraemonls <syyzls@gmail.com>
2017-01-07 21:20:40 +0800

    Update more markers on ditu.

--- web/ditu.js
+++ web/ditu.js
@@ -1,37 +1,46 @@
 var mapObj = new AMap.Map('mapcnt', {
   center: [116.39, 39.9],
-  zoom: 11
+  zoom: 8
 });
 
 var icon = new AMap.Icon({
-  image: 'http://vdata.amap.com/icons/b18/1/2.png', //24px*24px
-  size: new AMap.Size(24, 24)
+  size: new AMap.Size(16, 16)
 });
 
 var marker1 = new AMap.Marker({
   icon: icon, //24px*24px
-  position: [116.439092,39.944305],
-  offset: new AMap.Pixel(-12, -12),
+  position: [116.320265,39.894062],
   map: mapObj
 });
 
 var marker2 = new AMap.Marker({
   icon: icon, //24px*24px
   position: [116.488552,39.98057],
-  offset: new AMap.Pixel(-50, -75),
   map: mapObj
 });
 
 var marker3 = new AMap.Marker({
   icon: icon, //24px*24px
   position: [116.474894,40.012378],
-  offset: new AMap.Pixel(-100, -90),
   map: mapObj
 });
 
 var marker4 = new AMap.Marker({
   icon: icon, //24px*24px
   position: [116.460899,39.990007],
-  offset: new AMap.Pixel(-12, -123),
   map: mapObj
 });
+
+var marker5 = new AMap.Marker({
+  icon: icon, //24px*24px
+  position: [116.491926,39.926322],
+  map: mapObj
+});
+
+var marker6 = new AMap.Marker({
+  icon: icon, //24px*24px
+  position: [116.310319,39.97937],
+  map: mapObj
+});
+
+

c8e02231cf2f01db3e35a0d963cf4a4160f1004c
Doraemonls <syyzls@gmail.com>
2017-01-07 20:52:58 +0800

    Simplify ditu.

--- web/ditu.js
+++ web/ditu.js
@@ -1,15 +1,9 @@
 var mapObj = new AMap.Map('container', {
   center: [116.39, 39.9],
-  zoom: 11
-});
-
-var icon = new AMap.Icon({
-  image: 'http://vdata.amap.com/icons/b18/1/2.png', //24px*24px
-  size: new AMap.Size(24, 24)
+  zoom: 10
 });
 
 var marker1 = new AMap.Marker({
-  icon: icon, //24px*24px
   position: [116.439092, 39.944305],
   map: mapObj
 });

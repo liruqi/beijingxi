5cf3006aad7acf72827e6c1bac972d134e366087
Doraemonls <syyzls@gmail.com>
2017-01-07 21:04:32 +0800

    Fix ditumap.

--- web/ditu.js
+++ web/ditu.js
@@ -1,9 +1,37 @@
-var mapObj = new AMap.Map('container', {
+var mapObj = new AMap.Map('mapcnt', {
   center: [116.39, 39.9],
-  zoom: 10
+  zoom: 11
+});
+
+var icon = new AMap.Icon({
+  image: 'http://vdata.amap.com/icons/b18/1/2.png', //24px*24px
+  size: new AMap.Size(24, 24)
 });
 
 var marker1 = new AMap.Marker({
-  position: [116.439092, 39.944305],
+  icon: icon, //24px*24px
+  position: [116.439092,39.944305],
+  offset: new AMap.Pixel(-12, -12),
+  map: mapObj
+});
+
+var marker2 = new AMap.Marker({
+  icon: icon, //24px*24px
+  position: [116.488552,39.98057],
+  offset: new AMap.Pixel(-50, -75),
+  map: mapObj
+});
+
+var marker3 = new AMap.Marker({
+  icon: icon, //24px*24px
+  position: [116.474894,40.012378],
+  offset: new AMap.Pixel(-100, -90),
+  map: mapObj
+});
+
+var marker4 = new AMap.Marker({
+  icon: icon, //24px*24px
+  position: [116.460899,39.990007],
+  offset: new AMap.Pixel(-12, -123),
   map: mapObj
 });
